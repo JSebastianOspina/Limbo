bb5323c93a9237fb35aae7bf3e978536aa64bcd5
Miguel Cano <mcano0428@gmail.com>
2022-06-01 18:06:13 -0500

    Relations Champion, Software, Software_licences

--- app/Models/SoftwareLicense.php
+++ app/Models/SoftwareLicense.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class SoftwareLicense extends Model
 {
     use HasFactory;
+
+    public function software(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Software::class);
+    }
+
+    public function champions(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Champion::class);
+    }
+
 }
